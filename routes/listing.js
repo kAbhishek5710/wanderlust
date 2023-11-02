726e4c11934023c2bb5748ecd2094139724aca5f
Abhishek Kushwaha <kabhishek5710@gmail.com>
2023-11-02 17:14:57 +0530

    Adding Files initially

---
+++ routes/listing.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../Controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index)) //Index Route
+  .post(
+    //Create Route
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
+
+//New Route
+router.get("/new", isLoggedIn, wrapAsync(listingController.renderNewForm));
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing)) //Show ROute
+  .put(
+    //Update Route
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing)); //Delete Route
+
+//Edit Route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.editListing)
+);
+
+module.exports = router;
